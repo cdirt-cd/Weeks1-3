82d133f72d5baf32c9d6c78233b20c4c38d3a283
caden768 <caden768@gmail.com>
2025-01-22 10:46:57 -0500

    i forgot to save the visual studio stuff

---
+++ Assets/SpawningStuff.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningStuff : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
