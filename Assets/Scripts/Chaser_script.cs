1697542830394034fae44dac828d598e831cb055
caden768 <caden768@gmail.com>
2025-07-08 17:35:50 -0400

    Made the chaser follow the mouse but its stuck behind the camera. working on solution.

--- Assets/Scripts/Chaser_script.cs
+++ Assets/Scripts/Chaser_script.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Chaser_script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 newPosition;
+    public Camera gameCamera;
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePositionInWorldSpace = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePositionInWorldSpace.z = 0f;
+        transform.position = mousePositionInWorldSpace;
     }
 }
