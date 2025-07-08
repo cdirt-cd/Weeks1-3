cf9cb1bf0a964c9bfab9791d4ac23cda984c3510
caden768 <caden768@gmail.com>
2025-07-08 18:34:43 -0400

    Added debug log as well as comments stating what it is we want the code to do

--- Assets/Scripts/Chaser_script.cs
+++ Assets/Scripts/Chaser_script.cs
@@ -12,14 +12,14 @@ public class Chaser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Yo wassup");
     }
 
     // Update is called once per frame
     void Update()
     {
         
-
+        
 
         Vector3 mousePositionInWorldSpace = gameCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePositionInWorldSpace.z = 0f;
@@ -34,5 +34,23 @@ public class Chaser : MonoBehaviour
         Vector3 directionToMove = target - start;
 
         transform.position = transform.position + directionToMove * speed;
+
+        Vector3 chaserPositionInScreenSpace = gameCamera.WorldToScreenPoint(transform.position);
+
+        float xMin = 0f;
+        float yMin = 0f;
+        
+        float xMax = Screen.width;
+        float yMax = Screen.height;
+
+        //If chaser x is less than the xmin
+        //If chaser y is less than the ymin
+        //if chaser x is greater than the xmax
+        //if chaser y is greater than the ymax
+        //THEN:
+        //change the colour of the chaser to red
+
+
+
     }
 }
