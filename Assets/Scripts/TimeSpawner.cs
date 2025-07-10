39798034ce6bcf0cb8972d872df68d0f1769c20a
caden768 <caden768@gmail.com>
2025-07-10 19:23:57 -0400

    Added timespawner which spawns object in a timely manner!

---
+++ Assets/Scripts/TimeSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSpawner : MonoBehaviour
+{
+    public GameObject spawningPrefab;
+    public float spawnFrequency;
+
+    private float timeSinceLastSpawn = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        Debug.Log("Time since last spawn: " + timeSinceLastSpawn);
+
+        if (timeSinceLastSpawn > spawnFrequency)
+        {
+            Instantiate(spawningPrefab);
+            timeSinceLastSpawn = 0f;
+        }
+    }
+}
