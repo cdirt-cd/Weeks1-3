6576483390d784e79e0165f923e6cace279590bd
caden768 <caden768@gmail.com>
2025-07-10 18:14:48 -0400

    Creating new list and learning lists. For public lists Public List<GameObject> spawnedObjects;

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,14 +10,21 @@ public class Spawner : MonoBehaviour
     public GameObject spawningPrefab;
     public Vector3 spawnPoint;
 
+     List<GameObject> spawnedObjects = new List<GameObject>();
+    //List<int> numbers = new List<int>();
+
     // Start is called before the first frame update
     void Start()
     {
 
-        Gameobject spawnedObject = Instantiate(spawningPrefab);
-        Destroy(spawnedObject);
-        Gameobject spawnedObject2 = Instantiate(spawningPrefab, transform.position, Quaternion.identity);
-        Destroy(spawnedObject2, 3);
+        //spawnedObjects { }
+
+        GameObject spawnedObject = Instantiate(spawningPrefab);
+        spawnedObjects.Add(spawnedObject);
+        GameObject spawnedObject2 = Instantiate(spawningPrefab, transform.position, Quaternion.identity);
+        spawnedObjects.Add(spawnedObject2);
+
+        //spawnedObjects {spawnedObject, spawnedObject2 }
     }
 
     // Update is called once per frame
